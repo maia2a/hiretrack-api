7d9ae45c401ba29530ee778ea8ea0609a015d753
maia2a <gabrielldeveloper@gmail.com>
2025-07-24 14:51:28 -0300

    feat: add @clerk/express dependency and integrate clerkMiddleware into AppModule for authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,18 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { JobsModule } from './jobs/jobs.module';
 import { PrismaService } from './prisma/prisma.service';
+
+import { clerkMiddleware } from '@clerk/express';
 
 @Module({
   imports: [JobsModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(clerkMiddleware()).forRoutes('*');
+  }
+}
