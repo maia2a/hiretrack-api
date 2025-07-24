ec394940359fb4c2450656c3d0b4a339c155db16
maia2a <gabrielldeveloper@gmail.com>
2025-07-24 13:41:53 -0300

    feat: add class-transformer and class-validator dependencies; implement global validation and CORS in main.ts; enhance CreateJobDto with validation rules

--- src/jobs/dto/create-job.dto.ts
+++ src/jobs/dto/create-job.dto.ts
@@ -1,1 +1,16 @@
-export class CreateJobDto {}
+import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
+
+export class CreateJobDto {
+  @IsString({ message: 'O título deve ser um texto.' })
+  @IsNotEmpty({ message: 'O título é obrigatório.' })
+  @MaxLength(100)
+  title: string;
+
+  @IsString({ message: 'A descrição deve ser um texto.' })
+  @IsNotEmpty({ message: 'A descrição é obrigatória.' })
+  description: string;
+
+  @IsString({ message: 'A localização deve ser um texto.' })
+  @IsNotEmpty({ message: 'A localização é obrigatória.' })
+  location: string;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
