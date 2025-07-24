db122b55af94702c1750d536b71f60e21a0773b0
maia2a <gabrielldeveloper@gmail.com>
2025-07-24 13:30:13 -0300

    feat: integrate PrismaService into AppModule and add unit tests for PrismaService

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { JobsModule } from './jobs/jobs.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [JobsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
