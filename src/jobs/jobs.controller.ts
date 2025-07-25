29fddb1cac5104e4c73be295a06394f048238fc4
maia2a <gabrielldeveloper@gmail.com>
2025-07-25 11:08:08 -0300

    feat: enhance create job method in JobsController to include authentication check using Clerk

--- src/jobs/jobs.controller.ts
+++ src/jobs/jobs.controller.ts
@@ -1,3 +1,4 @@
+import { ExpressRequestWithAuth } from '@clerk/express';
 import {
   Body,
   Controller,
@@ -6,6 +7,8 @@ import {
   Param,
   Patch,
   Post,
+  Req,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { CreateJobDto } from './dto/create-job.dto';
 import { UpdateJobDto } from './dto/update-job.dto';
@@ -16,7 +19,15 @@ export class JobsController {
   constructor(private readonly jobsService: JobsService) {}
 
   @Post()
-  create(@Body() createJobDto: CreateJobDto) {
+  create(
+    @Body() createJobDto: CreateJobDto,
+    @Req() req: ExpressRequestWithAuth,
+  ) {
+    const { userId } = req.auth();
+    if (!userId) {
+      throw new UnauthorizedException('Você não está autenticado');
+    }
+    console.log(userId);
     return this.jobsService.create(createJobDto);
   }
 
